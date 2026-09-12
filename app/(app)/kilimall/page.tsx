d15a0105c86a9297da01613796b4a699b2912400
Brianogango <117631478+Brianogango@users.noreply.github.com>
2026-09-12 13:42:40 +0300

    Mark app/(app)/kilimall/page.tsx as client boundary for Next 15

--- app/(app)/kilimall/page.tsx
+++ app/(app)/kilimall/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 import { ModuleSkeleton } from '@/components/ui/ModuleSkeleton'
 const Kilimall = dynamic(() => import('@/components/modules/Kilimall'), { loading: () => <ModuleSkeleton />, ssr: false })
