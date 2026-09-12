5ac501f84d4a1aac4c55cdbc02fbd87792c53e61
Brianogango <117631478+Brianogango@users.noreply.github.com>
2026-09-12 13:42:53 +0300

    Mark app/(app)/reconfiguration/page.tsx as client boundary for Next 15

--- app/(app)/reconfiguration/page.tsx
+++ app/(app)/reconfiguration/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 import { ModuleSkeleton } from '@/components/ui/ModuleSkeleton'
 const Reconfiguration = dynamic(() => import('@/components/modules/Reconfiguration'), {
