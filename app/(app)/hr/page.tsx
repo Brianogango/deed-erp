6e721521782d1d064c925be8a483a456b2532418
Brianogango <117631478+Brianogango@users.noreply.github.com>
2026-09-12 13:42:21 +0300

    Mark app/(app)/hr/page.tsx as client boundary for Next 15

--- app/(app)/hr/page.tsx
+++ app/(app)/hr/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 import { ModuleSkeleton } from '@/components/ui/ModuleSkeleton'
 const HR = dynamic(() => import('@/components/modules/HR'), { loading: () => <ModuleSkeleton />, ssr: false })
