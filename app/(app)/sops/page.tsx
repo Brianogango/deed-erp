06c7989a36f5df3f887f9b72998bf631c0d00dd8
Brianogango <117631478+Brianogango@users.noreply.github.com>
2026-09-12 13:42:26 +0300

    Mark app/(app)/sops/page.tsx as client boundary for Next 15

--- app/(app)/sops/page.tsx
+++ app/(app)/sops/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 import { ModuleSkeleton } from '@/components/ui/ModuleSkeleton'
 const SOPs = dynamic(() => import('@/components/modules/SOPs'), { loading: () => <ModuleSkeleton />, ssr: false })
