2f61bffa3afc1aec34b1d24f68f0dc83ebe04de7
Brianogango <117631478+Brianogango@users.noreply.github.com>
2026-09-12 13:42:43 +0300

    Mark app/(app)/purchases/page.tsx as client boundary for Next 15

--- app/(app)/purchases/page.tsx
+++ app/(app)/purchases/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 import { ModuleSkeleton } from '@/components/ui/ModuleSkeleton'
 const Purchase = dynamic(() => import('@/components/modules/Purchase'), { loading: () => <ModuleSkeleton />, ssr: false })
